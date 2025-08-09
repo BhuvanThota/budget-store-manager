cc748286c5bc769bf4108da2be2f07ed035a1d5d
Teacher B <bhuvan.thota3@gmail.com>
2025-08-09 15:07:21 +0530

    Updated Prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+// src/lib/prisma.ts
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
+  globalThis.prisma = prisma;
 }
