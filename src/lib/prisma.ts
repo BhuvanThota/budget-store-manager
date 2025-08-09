ac1f63a94fc4bc427d48bae3fda3d27492bb7242
Teacher B <bhuvan.thota3@gmail.com>
2025-08-09 14:48:02 +0530

    Revert "Updated Prisma"
    This reverts commit daa594e2dde93999789889336390c89f13e2e76b.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,11 @@
-// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from '@prisma/client';
-
-// This declares a global variable to hold the Prisma Client instance.
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-// This line either creates a new PrismaClient instance or reuses the existing one from the global scope.
-// The `globalThis.prisma` part is crucial for preventing new instances from being created during hot-reloads in development.
-export const prisma = globalThis.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }
